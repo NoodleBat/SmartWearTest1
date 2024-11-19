ef8fbdb64b343991ecf6b77ca821ff0ae39206ec
Asus <Asus@DESKTOP-9ER4931>
2024-11-19 22:33:15 +0300

    Добавьте файлы проекта.

---
+++ SmartWearTest1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartWearTest1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SmartWearTest1/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartWearTest1
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
